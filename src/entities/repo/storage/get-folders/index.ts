c4df568ac3d01ff837dcd7e384d31ea7d380abec
gr421nidan <nikushkina.dasha@yandex.ru>
2025-03-26 18:46:29 +0700

    да

--- src/entities/repo/storage/get-folders/index.ts
+++ src/entities/repo/storage/get-folders/index.ts
@@ -19,6 +19,36 @@ const mockFolders: IGetStorageFoldersDto[] = [
         title: "Папка 3",
         size: 0,
     },
+    {
+        id: generateUUID(),
+        title: "Папка 1",
+        size: 1048576,
+    },
+    {
+        id: generateUUID(),
+        title: "Папка 2",
+        size: 1048575,
+    },
+    {
+        id: generateUUID(),
+        title: "Папка 3",
+        size: 0,
+    },
+    {
+        id: generateUUID(),
+        title: "Папка 1",
+        size: 1048576,
+    },
+    {
+        id: generateUUID(),
+        title: "Папка 2",
+        size: 1048575,
+    },
+    {
+        id: generateUUID(),
+        title: "Папка 3",
+        size: 0,
+    },
 ];
 
 const getStorageFoldersRepository = async (): Promise<IGetStorageFoldersDto[]> => {
---
+++ src/shared/type/user/update-user-profile/index.ts
@@ -0,0 +1,6 @@
+import {IUpdateUserPhotoPort, IUpdateUserPort} from "@/shared/interface/user";
+
+type IFormUpdateUserData = IUpdateUserPort
+type IFormUpdatePhotoData = IUpdateUserPhotoPort;
+
+export type {IFormUpdateUserData, IFormUpdatePhotoData};
