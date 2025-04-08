af40c9961d57d25201c37d2615962854889b6d2a
gr421nidan <nikushkina.dasha@yandex.ru>
2025-04-08 18:50:56 +0700

    да...

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -1,14 +1,39 @@
 import {createBrowserRouter} from "react-router-dom";
 import {
-    LazyAvailableStoragePage, LazyAvailableStoragesPage, LazyForbiddenPage, LazyMainPage, LazyNotFoundPage,
-    LazyStorageTrashPage, LazyUserProfilePage
+    LazyAvailableStoragePage,
+    LazyAvailableStoragesPage, LazyConnectingStoragePage, LazyForbiddenPage,
+    LazyMainPage, LazyNotFoundPage,
+    LazyResetPage,
+    LazySignInPage,
+    LazySignUpPage, LazyStorageSettingsPage, LazyStorageTrashPage,
+    LazyStorageUsersPage,
+    LazyUserLogsPage, LazyUserProfilePage
 } from "@/pages";
 import ERouterPath from "@/shared/common/enum/router";
+import AuthLayout from "@/shared/components/layouts/auth";
 import MainLayout from "@/shared/components/layouts/main";
 import ProtectedRoute from "@/shared/components/protected-router";
+import ConnectingStorageLayout from "../../shared/components/layouts/main-secondary";
 import {ERoleID} from "@/shared/enum/auth";
 
 const router = createBrowserRouter([
+    {
+        element: <AuthLayout/>,
+        children: [
+            {
+                path: ERouterPath.SIGN_IN_PAGE,
+                element: <LazySignInPage/>,
+            },
+            {
+                path: ERouterPath.SIGN_UP_PAGE,
+                element: <LazySignUpPage/>,
+            },
+            {
+                path: ERouterPath.RESET_PAGE,
+                element: <LazyResetPage/>,
+            },
+        ],
+    },
     {
         element: <ProtectedRoute allowedRoles={[ERoleID.USER, ERoleID.ADMIN]}/>,
         children: [
@@ -32,6 +57,28 @@ const router = createBrowserRouter([
         ],
     },
     {
+        element: <ProtectedRoute allowedRoles={[ERoleID.ADMIN]}/>,
+        children: [
+            {
+                element: <MainLayout/>,
+                children: [
+                    {
+                        path: ERouterPath.USERS,
+                        element: <LazyStorageUsersPage/>,
+                    },
+                    {
+                        path: ERouterPath.USER_LOGS,
+                        element: <LazyUserLogsPage/>,
+                    },
+                    {
+                        path: ERouterPath.STORAGE_SETTINGS,
+                        element: <LazyStorageSettingsPage/>,
+                    },
+                ],
+            },
+        ],
+    },
+    {
         element: <ProtectedRoute allowedRoles={[ERoleID.USER]}/>,
         children: [
             {
@@ -44,6 +91,15 @@ const router = createBrowserRouter([
                     {
                         path: ERouterPath.STORAGE,
                         element: <LazyAvailableStoragePage/>,
+                    },
+                ],
+            },
+            {
+                element: <ConnectingStorageLayout/>,
+                children: [
+                    {
+                        path: ERouterPath.CONNECTING_STORAGE,
+                        element: <LazyConnectingStoragePage/>,
                     },
                 ],
             },
