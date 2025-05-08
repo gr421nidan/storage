14268ac7263441dd5a9ec55095e751b64ff4157d
jahongir2503 <jojo.isomidinov@gmail.com>
2025-05-08 16:53:12 +0700

    подкрученные функци к ui

--- src/entities/repo/backup/cleanup/index.ts
+++ src/entities/repo/backup/cleanup/index.ts
@@ -1,9 +1,9 @@
-import {api} from "@/shared/api";
-import {IActionBackupDto} from "@/shared/interface/backup";
+import { api } from '@/shared/api';
+import { IActionBackupDto } from '@/shared/interface/backup';
 
-const cleanupBackupsRepository= async (storageId: string): Promise<IActionBackupDto> => {
-    const response = await api.delete<IActionBackupDto>(`/backup/del/${storageId}`);
+const cleanupBackupsRepository = async (storageId: string): Promise<IActionBackupDto> => {
+    const response = await api.delete<IActionBackupDto>(`/backup/delete/${storageId}`);
     return response.data;
-
 };
+
 export default cleanupBackupsRepository;
--- src/pages/admin/storage-settings/index.tsx
+++ src/pages/admin/storage-settings/index.tsx
@@ -1,80 +1,54 @@
-import {ReactNode, useState} from 'react';
+import { ReactNode, useState } from 'react';
 import Button from '@/shared/components/buttons/button';
 import ButtonIcon from '@/shared/components/buttons/button-icon';
-import {BackupStatus} from '@/features/admin/storage-settings/backup-status/ui';
+import { BackupStatus } from '@/features/admin/storage-settings/backup-status/ui';
 import BackupList from '@/features/admin/storage-settings/backup-table/ui';
+import StorageSettingsForm from '@/features/admin/storage-settings/storege-form/ui';
+import CleanupBackupsConfirm from '@/features/admin/storage-settings/cleanup-backups-confirm';
+import useGetBackupsUseCase from '@/entities/cases/backup/get-backups/use-case';
+import useCreateBackupPresenter from '@/entities/cases/backup/create/presenter';
 import styles from './style';
-import StorageSettingsForm from "@/features/admin/storage-settings/storege-form/ui";
-import CleanupBackupsConfirm from "@/features/admin/storage-settings/cleanup-backups-confirm";
-import useGetBackupsUseCase from "@/entities/cases/backup/get-backups/use-case";
 
 const StorageSettingsPage = (): ReactNode => {
     const [isActive, setIsActive] = useState(true);
     const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
-    const {data: backups} = useGetBackupsUseCase();
+    const { data: backups } = useGetBackupsUseCase();
     const isDisabled = !backups.length;
-    const handleOpenDeleteModal = () => {
-        setIsDeleteModalOpen(true);
-    };
+    const { handleBackup } = useCreateBackupPresenter();
 
-    const handleCloseDeleteModal = () => {
-        setIsDeleteModalOpen(false);
-    };
     return (
         <>
-            <StorageSettingsForm/>
+            <StorageSettingsForm />
             <div className={styles.sectionWrapper}>
-                <div className={styles.sectionWrapp}>
-                    <div className={styles.sectionTitle}>Состояние хранилища</div>
-                    <div className={styles.stateWrapper}>
-                        <div className={styles.stateText}>
-                            {isActive ? 'Активно' : 'Неактивно'}
-                        </div>
-                        <ButtonIcon
-                            icon={
-                                isActive
-                                    ? 'iconamoon:lock-light'
-                                    : 'iconamoon:lock-off-light'
-                            }
-                            className={styles.iconStyles}
-                            onClick={() => setIsActive(!isActive)}
-                        />
+                <div className={styles.sectionTitle}>Состояние хранилища</div>
+                <div className={styles.stateWrapper}>
+                    <div className={styles.stateText}>{isActive ? 'Активно' : 'Неактивно'}</div>
+                    <ButtonIcon
+                        icon={isActive ? 'iconamoon:lock-off-light' : 'iconamoon:lock-light'}
+                        className={styles.iconStyles}
+                        onClick={() => setIsActive(!isActive)}
+                    />
                 </div>
-                </div>
-                <div className={styles.divider}/>
             </div>
-            <div>
                 <div className={styles.verticalFlow}>
-                    <BackupStatus/>
-
+                    <BackupStatus />
                     <div className={styles.row}>
-            <span className={styles.label}>
-              Следующее резервное копирование: 21.06.2025
-            </span>
-                        <Button className="h-[54px] w-[253px]">Создать копию</Button>
+                        <span className={styles.label}>Следующее резервное копирование: 21.06.2025</span>
+                        <Button onClick={handleBackup} className="h-[54px] w-[253px]">
+                            Создать копию
+                        </Button>
                     </div>
-
-                    <div className={styles.divider}/>
-
+                    <div className={styles.divider} />
                     <div className={styles.row}>
-            <span className={styles.label}>
-              Управление резервными копиями
-            </span>
-                        <Button className="h-[54px] w-[415px]" onClick={handleOpenDeleteModal} disabled={isDisabled}>
+                        <span className={styles.label}>Управление резервными копиями</span>
+                        <Button onClick={() => setIsDeleteModalOpen(true)} disabled={isDisabled} className="h-[54px] w-[415px]">
                             Очистить список резервных копий
                         </Button>
                     </div>
-
-                    <span className={styles.label}>
-            Созданные резервные копии:
-          </span>
-                    <BackupList/>
+                    <span className={styles.label}>Созданные резервные копии:</span>
+                    <BackupList />
                 </div>
-            </div>
-            <CleanupBackupsConfirm
-                isOpen={isDeleteModalOpen}
-                onClose={handleCloseDeleteModal}
-            />
+            <CleanupBackupsConfirm isOpen={isDeleteModalOpen} onClose={() => setIsDeleteModalOpen(false)} />
         </>
     );
 };
